f97c48ae2b743e29262459d21844e4a8573e4612
philefua <philefua@gmail.com>
2026-07-13 04:41:56 +0100

    Update business_settings.dart

--- lib/models/business_settings.dart
+++ lib/models/business_settings.dart
@@ -1,23 +1,61 @@
-class BusinessSettings {
+import 'package:hive/hive.dart';
+
+part 'business_settings.g.dart';
+
+@HiveType(typeId: 0)
+class BusinessSettings extends HiveObject {
+  @HiveField(0)
   String businessName;
+
+  @HiveField(1)
   String address;
-  String whatsapp;
-  String website;
-  String instagram;
-  String facebook;
-  String footnote;
+
+  @HiveField(2)
+  String phone;
+
+  @HiveField(3)
+  String? logoPath;
+
+  @HiveField(4)
   String currencySymbol;
-  String managerPassword;
+
+  @HiveField(5)
+  String managerPasswordHash;
+
+  @HiveField(6)
+  int lastReceiptCounterDate;
+
+  @HiveField(7)
+  int dailyReceiptCounter;
+
+  @HiveField(8)
+  String whatsapp;
+
+  @HiveField(9)
+  String website;
+
+  @HiveField(10)
+  String instagram;
+
+  @HiveField(11)
+  String facebook;
+
+  @HiveField(12)
+  String footnote;
 
   BusinessSettings({
-    this.businessName = "Printiverse",
-    this.address = "Benin City, Edo State, Nigeria",
-    this.whatsapp = "",
-    this.website = "",
-    this.instagram = "",
-    this.facebook = "",
-    this.footnote = "Thank you for your patronage!",
-    this.currencySymbol = "₦",
-    this.managerPassword = "admin", // Default password
+    this.businessName = 'My Business',
+    this.address = '',
+    this.phone = '',
+    this.logoPath,
+    this.currencySymbol = '\$',
+    required this.managerPasswordHash,
+    this.lastReceiptCounterDate = 0,
+    this.dailyReceiptCounter = 0,
+    this.whatsapp = '',
+    this.website = '',
+    this.instagram = '',
+    this.facebook = '',
+    this.footnote = '',
   });
 }
